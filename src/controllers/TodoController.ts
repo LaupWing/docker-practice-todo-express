b63fa9e137264ac653ddd20c899be27bbb46a3e5
laupwing <loc_strict@hotmail.com>
2023-12-22 08:52:19 +0100

    added base classes

---
+++ src/controllers/TodoController.ts
@@ -0,0 +1,19 @@
+
+
+export default class TodoController {
+   get = async () => {
+
+   }
+
+   create = async () => {
+
+   }
+
+   update = async () => {
+
+   }
+   
+   all = async () => {
+
+   }
+}
