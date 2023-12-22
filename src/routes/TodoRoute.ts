1a3cb99c54f53996442226479a2f547ebb517d20
laupwing <loc_strict@hotmail.com>
2023-12-22 14:22:51 +0100

    updated

--- src/routes/TodoRoute.ts
+++ src/routes/TodoRoute.ts
@@ -1,11 +1,15 @@
 import { Application, Router } from "express"
+import TodoController from "../controllers/TodoController"
 
 
 export default class TodoRoute {
    router = Router()
+   controller = new TodoController()
 
    constructor(app: Application) {
-
-
+      this.router = app.get("/all", this.controller.all)
+      this.router = app.get("/get/:id", this.controller.get)
+      this.router = app.patch("/get/:id", this.controller.update)
+      this.router = app.post("/create", this.controller.create)
    }
 }
