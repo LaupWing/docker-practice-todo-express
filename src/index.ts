2474b41c6e064a9e8f33cba1ed6d756ab9a59d73
laupwing <loc_strict@hotmail.com>
2024-02-25 10:10:11 +0100

    Refactor server initialization and add dynamic port assignment

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from "@prisma/client"
 import type { Application } from "express"
 import express from "express"
+import Server from "./server"
 
 const app: Application = express()
+new Server(app)
+const PORT: number = process.env.PORT ? Number(process.env.PORT) : 8080
 const prisma = new PrismaClient()
 
-app
-   .get("/", async (req, res) => {
-      const todos = await prisma.todo.findMany()
-      res.json({
-         message: "Hello World",
-         todos
-      })
-   })
-   .listen(3000, () => console.log("Server is running on http://localhost:3000"))
+app.get("/", async (req, res) => {
+    const todos = await prisma.todo.findMany()
+    res.json({
+        message: "Hello World",
+        todos,
+    })
+}).listen(PORT, () => console.log("Server is running on http://localhost:3000"))
