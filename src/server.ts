6ce28d39aa013b7d5a181facba57bb9bcc55aad4
laupwing <loc_strict@hotmail.com>
2024-02-25 10:14:21 +0100

    Add Routes to Server config

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
 import type { Application } from "express"
 import express from "express"
 import cors from "cors"
+import Routes from "./routes"
 
 export default class Server {
-   constructor(app: Application) {
-      this.config(app)
-   }
+    constructor(app: Application) {
+        this.config(app)
+        new Routes(app)
+    }
 
-   private config(app: Application) {
-      const cors_options = {
-         origin: "http://localhost:8081",
-      }
+    private config(app: Application) {
+        const cors_options = {
+            origin: "http://localhost:8081",
+        }
 
-      app.use(express.json({ limit: "50mb" }))
-      app.use(cors(cors_options))
-   }
+        app.use(express.json({ limit: "50mb" }))
+        app.use(cors(cors_options))
+    }
 }
