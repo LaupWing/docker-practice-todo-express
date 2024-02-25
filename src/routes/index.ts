daaa3efc09ddbcb296c5dc15832bc617823b3454
laupwing <loc_strict@hotmail.com>
2024-02-25 10:18:21 +0100

    Remove TodoRoute and update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import type { Application } from "express"
+import todoRoutes from "./TodoRoutes"
 
 export default class Routes {
     constructor(app: Application) {
-        app.use("/todos")
+        app.use("/todos", todoRoutes)
     }
 }
