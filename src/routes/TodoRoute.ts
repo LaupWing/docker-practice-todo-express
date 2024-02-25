713c7c6a0a0304868ea6c1c7fb498294d3a4a081
laupwing <loc_strict@hotmail.com>
2024-02-25 10:13:05 +0100

    Refactor TodoRoute class

--- src/routes/TodoRoute.ts
+++ src/routes/TodoRoute.ts
@@ -1,17 +1,16 @@
 import { Router } from "express"
 import TodoController from "../controllers/TodoController"
 
-
 export class TodoRoute {
-   router = Router()
-   controller = new TodoController()
+    router = Router()
+    controller = new TodoController()
 
-   constructor() {
-      this.router.get("/all", this.controller.all)
-      this.router.get("/get/:id", this.controller.get)
-      this.router.patch("/get/:id", this.controller.update)
-      this.router.post("/create", this.controller.create)
-   }
+    constructor() {
+        this.router.get("/all", this.controller.all)
+        this.router.get("/get/:id", this.controller.get)
+        this.router.patch("/get/:id", this.controller.update)
+        this.router.post("/create", this.controller.create)
+    }
 }
 
 export default new TodoRoute().router
