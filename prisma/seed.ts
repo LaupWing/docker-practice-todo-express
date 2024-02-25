3d31aa1c52302b24b690ba1590befb9d0c055663
laupwing <loc_strict@hotmail.com>
2024-02-25 10:01:28 +0100

    Refactor seed.ts file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,20 +3,20 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-   await prisma.todo.create({
-      data: {
-         title: "Buy milk",
-         completed: false,
-      }
-   })
+    await prisma.todo.create({
+        data: {
+            title: "Buy milk",
+            completed: false,
+        },
+    })
 }
 
 main()
-   .then(async () => {
-      await prisma.$disconnect()
-   })
-   .catch(async (e) => {
-      console.error(e)
-      await prisma.$disconnect()
-      process.exit(1)
-   })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
